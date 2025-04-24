35f4ac993e65db9a851c6cc6a01f341d0de2f814
Richardzik <kubrama04@gmail.com>
2025-04-24 21:47:21 +0200

    Minor design changes for the main page

--- src/entity/Pokemon.jsx
+++ src/entity/Pokemon.jsx
@@ -3,7 +3,7 @@ class Pokemon {
       this.name = name;
       this.id = id;
       this.type = types[0].type.name; // primary type
-      this.image = id <= 649
+      this.image = id <= 649 // Animated sprites are only available for the first 649 Pokemon then the static image needs to be used
       ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`
       : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
       this.abilities = abilities.map(a => a.ability.name);
